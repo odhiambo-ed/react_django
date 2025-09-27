79fcd063646f966716dec582d4677405442533a6
odhiambo_ed <odhiambo.edward111@gmail.com>
2025-09-27 14:36:54 +0300

    add create_book view and update URL routing for book management

--- server/project/api/views.py
+++ server/project/api/views.py
@@ -9,3 +9,11 @@ def get_books(request):
     books = Book.objects.all()
     serializedData = BookSerializer(books, many=True).data
     return Response(serializedData)
+
+@api_view(['POST'])
+def create_book(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
