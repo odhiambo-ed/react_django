3935643a794fdb23c42a58fe6c524a6e059d49dd
odhiambo_ed <odhiambo.edward111@gmail.com>
2025-09-27 13:54:35 +0300

    add book view

--- server/project/api/views.py
+++ server/project/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decotors import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from api.models import Book
+from api.serializer import BookSerializer 
 
 # Create your views here.
